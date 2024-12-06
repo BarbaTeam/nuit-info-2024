d6dbd6785634570f3922c59d476f63b0a4c5cbf1
zaynab-nachabe <nachabezaynab5@gmail.com>
2024-12-06 01:29:50 +0100

    buyer update can purchase a cargo

--- front/js/utils/buyer.js
+++ front/js/utils/buyer.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const zodiakButton = document.querySelector('.money-maker-facility-add');
+    const cargoButton = document.querySelector('.cargo-facility-add');
+    
     zodiakButton.addEventListener('click', async function() {
         try {
             const response = await fetch('/buy_zodiak', {
@@ -18,4 +20,23 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error:', error);
         }
     });
+    cargoButton.addEventListener('click', async function() {
+        try{
+            const response = await fetch('/buy_cargo', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            if (response.ok) {
+                alert('Cargo purchased! Remaining money: ' + data.money);
+            } else {
+                alert(data.error);
+            }
+        }catch (error){
+            console.error('Eror: ', error);
+        }
+    });
+    
 });
